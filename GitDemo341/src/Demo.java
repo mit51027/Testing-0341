7120475052a2d5e4da516e3acb8554f91f2442b5
MANISH <MANISH@DESKTOP-NPV50CU>
2022-09-22 14:22:14 +0530

    chnages by amit

--- GitDemo341/src/Demo.java
+++ GitDemo341/src/Demo.java
@@ -13,10 +13,6 @@ public void m2()
 System.out.println(" hello m2 method ");
 }
 
-public void m3()
-{
-System.out.println(" hello m3 method ");
-}
 
 public void sum()
 {
@@ -37,7 +33,7 @@ Demo a=new Demo();
 
 a.m1();
 a.m2();
-a.m3();
+
 a.sum();
 a.mul();
 
